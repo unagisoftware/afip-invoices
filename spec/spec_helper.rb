c016ff835d0176313d7ae635b56f9c971c6b3438
guidocollino <collinoguido@gmail.com>
2021-09-13 10:02:26 -0300

    Add rubocop and coverage badges in README (#5)
    Co-authored-by: Guido Collino <guido.collino@aerolaplata.com.ar>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,23 @@ require 'should_not/rspec'
 require 'shoulda/matchers'
 require 'webmock/rspec'
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |c|
+  c.report_with_single_file = true
+  c.single_report_path = 'coverage/lcov.info'
+end
 
 SimpleCov.start 'rails' do
   add_filter '/app/uploaders/'
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/'
+
+  SimpleCov.formatter =
+  SimpleCov::Formatter::MultiFormatter.new \
+    [SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::LcovFormatter]
 end
 
 RSpec.configure do |config|
